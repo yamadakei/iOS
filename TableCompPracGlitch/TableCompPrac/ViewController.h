9b475f8a591b68b08f44f40ee92dee3a24abe595
yamadakei <yamadakei1979@gmail.com>
2012-12-20 14:37:14 +0900

    Revert "Revert "Revert "Revert "Add TableCompPracGlitch""""
    This reverts commit f3be9e48faa0c7799ada5d5b4ab923dbc34e4c3d.

---
+++ TableCompPracGlitch/TableCompPrac/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  TableCompPrac
+//
+//  Created by 山田 慶 on 2012/11/20.
+//  Copyright (c) 2012年 山田 慶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    
+}
+
+@end
