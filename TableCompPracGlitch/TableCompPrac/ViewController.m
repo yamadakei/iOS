1e1b02c4c67234a3c0b1b1489882266d263c68d1
yamadakei <yamadakei1979@gmail.com>
2012-12-20 14:09:07 +0900

    Add TableCompPracGlitch

---
+++ TableCompPracGlitch/TableCompPrac/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TableCompPrac
+//
+//  Created by 山田 慶 on 2012/11/20.
+//  Copyright (c) 2012年 山田 慶. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TableCompPracGlitch/TableCompPrac/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableCompPrac
+//
+//  Created by 山田 慶 on 2012/11/20.
+//  Copyright (c) 2012年 山田 慶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
