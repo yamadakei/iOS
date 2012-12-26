d60588726fa1430430843ab5c4fdc71685e5cb22
yamadakei <yamadakei1979@gmail.com>
2012-12-26 19:24:22 +0900

    サーバーにイメージを保存するとこまで出来た

--- UploadImageToWeb/UploadImageToWeb/ViewController.m
+++ UploadImageToWeb/UploadImageToWeb/ViewController.m
@@ -19,13 +19,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSURL *url = [NSURL URLWithString:@"http://codemafia.asia/~ldqy/test.php"];
+    [self upload];
+    
+    NSURL *url = [NSURL URLWithString:@"http://codemafia.asia/~ldqy/displayImage.php"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     
     request.HTTPMethod = @"POST";
-    
-    NSString *body = [NSString stringWithFormat:@"a=testA&b=testB&c=testC"];
-    request.HTTPBody = [body dataUsingEncoding:NSUTF8StringEncoding];
     
     [webView loadRequest:request];
 }
@@ -34,6 +33,41 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) upload {
+    NSString *urlString = @"http://codemafia.asia/~ldqy/image1.php";
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:urlString]];
+    [request setHTTPMethod:@"POST"];
+    
+    NSMutableData *body = [NSMutableData data];
+    
+    NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
+    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+    
+    // file
+    UIImage *image = [UIImage imageNamed:@"full2.jpg"];
+    NSData *imageData = UIImageJPEGRepresentation(image, 90);
+    
+    [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithString:@"Content-Disposition: attachment; name=\"userfile\"; filename=\"full2.jpg\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[NSData dataWithData:imageData]];
+    [body appendData:[[NSString stringWithString:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // close form
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // set request body
+    [request setHTTPBody:body];
+    
+    //return and test
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"%@", returnString);
 }
 
 @end
