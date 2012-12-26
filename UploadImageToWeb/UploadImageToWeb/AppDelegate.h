c806eede33239286dbd7c4d61ffcd9c48f56c8e9
yamadakei <yamadakei1979@gmail.com>
2012-12-26 16:55:08 +0900

    画像アップロード用プロジェクト追加

---
+++ UploadImageToWeb/UploadImageToWeb/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UploadImageToWeb
+//
+//  Created by 山田 慶 on 2012/12/26.
+//  Copyright (c) 2012年 山田 慶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UploadImageToWeb/UploadImageToWeb/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UploadImageToWeb
+//
+//  Created by 山田 慶 on 2012/12/26.
+//  Copyright (c) 2012年 山田 慶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
