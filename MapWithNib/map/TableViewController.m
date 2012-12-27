4eb135dec2363a0981cacfb9d22a10364e39f96e
yamadakei <yamadakei1979@gmail.com>
2012-12-27 17:46:21 +0900

    activity view問題をgcdを使い解決できないか模索開始

--- MapWithNib/map/TableViewController.m
+++ MapWithNib/map/TableViewController.m
@@ -106,6 +106,14 @@
     
     [activityView startAnimating];
     
+    dispatch_queue_t queue =
+    dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_group_async(group, queue, ^{NSLog(@"blk0");});
+    dispatch_group_async(group, queue, ^{NSLog(@"blk1");});
+    dispatch_group_async(group, queue, ^{NSLog(@"blk2");});
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    
     MKCoordinateRegion rangeCoord = customCell.viewForMap.map.region;
     rangeCoord.center = loc;
     [customCell.viewForMap.map setCenterCoordinate:loc animated:YES];
